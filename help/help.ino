1494ca062b83df0bdc1843adc4455af0fc330cf2
Dylan Jones <dylanjones2011@gmail.com>
2022-11-18 23:45:05 -0500

    Add Stepper class based on ledc API

--- help/help.ino
+++ help/help.ino
@@ -3,21 +3,17 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include "I2Cdev.h"
-//#include "MPU6050.h"
 #include "MPU6050_6Axis_MotionApps_V6_12.h"
 
-#define PIN_IN3  32
-#define PIN_IN4  33
-#define PIN_IN1  27 // ESP32 pin GIOP27 connected to the IN1 pin L298N
-#define PIN_IN2  26 // ESP32 pin GIOP26 connected to the IN2 pin L298N
-#define PIN_ENA  14 // ESP32 pin GIOP14 connected to the EN1 pin L298N
-#define PIN_ENB  12
-#define PIN_TRIMPOT 25
-#define PIN_DTRIM 35
+#define PIN_DIRA  32
+#define PIN_STEPA  33
+#define PIN_DIRB  26
+#define PIN_STEPB  27
 #define PIN_MPUINT 18
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define DEVICE_NAME "C4 Cat"
 
 class PIDController {
   private:
@@ -47,59 +43,98 @@ class PIDController {
 };
 
 
-class TwoWayMotor {
+//class TwoWayMotor {
+//  private:
+//    int dirPinA, dirPinB, enPin;
+//  public:
+//    TwoWayMotor(int dirPinA, int dirPinB, int enPin):
+//      dirPinA(dirPinA), dirPinB(dirPinB), enPin(enPin) {
+//      }
+//
+//      void init() {
+//        pinMode(dirPinA, OUTPUT);
+//        pinMode(dirPinB, OUTPUT);
+//        pinMode(enPin, OUTPUT);
+//        digitalWrite(dirPinA, LOW);
+//        digitalWrite(dirPinB, LOW);
+//        digitalWrite(enPin, LOW);
+//      }
+//
+//      void write(int amount){
+//        if (amount < -255 || amount > 255) {
+//          // Serial.println("OUT OF RANGE URBAD");
+//          return;
+//        }
+//        if (amount < 0) {
+//          // Serial.println("Below zero");
+//          digitalWrite(dirPinA, HIGH);
+//          digitalWrite(dirPinB, LOW);
+//          amount *= -1;
+//        } else if (amount > 0) {
+//          // Serial.println("Banove zero");
+//          digitalWrite(dirPinA, LOW);
+//          digitalWrite(dirPinB, HIGH);
+//        } else {
+//          // Serial.println("ZEORKEORKOESR");
+//          digitalWrite(dirPinA, LOW);
+//          digitalWrite(dirPinB, LOW);
+//        }
+//        // Serial.print("Marco is: "); // Serial.println(amount);
+//        analogWrite(enPin, amount);
+//      }
+//};
+
+// don't make too many of these
+int channelIdx = 0;
+/**
+ * Stepper motor abstraction.  Input range is from
+ * -255 to 255; maxStepRate is in Hz.
+ */
+class Stepper {
   private:
-    int dirPinA, dirPinB, enPin;
+    const int step, dir, ledChannel, maxStepRate;
   public:
-    TwoWayMotor(int dirPinA, int dirPinB, int enPin):
-      dirPinA(dirPinA), dirPinB(dirPinB), enPin(enPin) {
-      }
+    Stepper(int step, int dir, int maxStepRate) :
+      step(step), dir(dir), ledChannel(channelIdx++), maxStepRate(maxStepRate) {
+    }
 
-      void init() {
-        pinMode(dirPinA, OUTPUT);
-        pinMode(dirPinB, OUTPUT);
-        pinMode(enPin, OUTPUT);
-        digitalWrite(dirPinA, LOW);
-        digitalWrite(dirPinB, LOW);
-        digitalWrite(enPin, LOW);
-      }
+    void init() {
+      pinMode(dir, OUTPUT);
+      pinMode(step, OUTPUT);
+      digitalWrite(step, LOW);
+      digitalWrite(dir, LOW);
+      // Default freq of 500Hz
+      ledcSetup(ledChannel, 500, 10);
+      ledcAttachPin(step, ledChannel);
+      // Write 0 to stop any output
+      ledcWrite(ledChannel, 0);
+    }
 
-      void write(int amount){
-        if (amount < -255 || amount > 255) {
-          // Serial.println("OUT OF RANGE URBAD");
-          return;
-        }
-        if (amount < 0) {
-          // Serial.println("Below zero");
-          digitalWrite(dirPinA, HIGH);
-          digitalWrite(dirPinB, LOW);
-          amount *= -1;
-        } else if (amount > 0) {
-          // Serial.println("Banove zero");
-          digitalWrite(dirPinA, LOW);
-          digitalWrite(dirPinB, HIGH);
-        } else {
-          // Serial.println("ZEORKEORKOESR");
-          digitalWrite(dirPinA, LOW);
-          digitalWrite(dirPinB, LOW);
-        }
-        // Serial.print("Marco is: "); // Serial.println(amount);
-        analogWrite(enPin, amount);
+    void write (int amount) {
+      digitalWrite(dir, amount < 0);
+      if (amount == 0) {
+        ledcWrite(ledChannel, 0);
+        return;
       }
+      amount = abs(amount);
+      // Change freq and set duty cycle to 1.
+      int requestedFreq = (int)(((float) amount / 255.0f) * maxStepRate);
+      int ra = ledcChangeFrequency(ledChannel, requestedFreq, 10);
+      ledcWrite(ledChannel, 2000);
+      Serial.printf("lc: %d, rf: %d, ret1: %d\n", ledChannel, requestedFreq, ra);
+    }
 };
-
-double mapf(double val, double in_min, double in_max, double out_min, double out_max) {
-    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
   mpuInterrupt = true;
 }
 
-TwoWayMotor left(PIN_IN1, PIN_IN2, PIN_ENA);
-TwoWayMotor right(PIN_IN4, PIN_IN3, PIN_ENB);
+//TwoWayMotor left(PIN_IN1, PIN_IN2, PIN_ENA);
+//TwoWayMotor right(PIN_IN4, PIN_IN3, PIN_ENB);
+Stepper left(PIN_STEPA, PIN_DIRA, 200);
+Stepper right(PIN_STEPB, PIN_DIRB, 200);
+
 // Roughly 1/64 conversion factor
 //                       p,     i, d,  target
 PIDController controller(0.012, 0, 0, 0);
@@ -194,7 +229,7 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 void bleInit() {
   //////// BLE init
   // Create the BLE Device
-  BLEDevice::init("C4 Cat");
+  BLEDevice::init(DEVICE_NAME);
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
@@ -252,12 +287,6 @@ void mpuInit() {
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-//  // wait for ready
-//  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-//  while (Serial.available() && Serial.read()); // empty buffer
-//  while (!Serial.available());                 // wait for data
-//  while (Serial.available() && Serial.read()); // empty buffer again
-
   // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
@@ -307,56 +336,40 @@ void mpuInit() {
 void setup() {
   Wire.begin();
   Wire.setClock(400000);
-  Serial.begin(38400);
-  pinMode(PIN_TRIMPOT, INPUT);
-  pinMode(PIN_DTRIM, INPUT);
+  Serial.begin(115200);
   
-  // initialize device
+  // initialize devices
   Serial.println("Initializing I2C devices...");
   mpuInit();
+  Serial.println("Initializing BLE...");
   bleInit();
-  
-  // configure Arduino LED pin for output
-  // pinMode(LED_PIN, OUTPUT);
   left.init();
   right.init();
 }
 
 int lastMillis = 0;
 void loop() {
-  // read raw accel/gyro measurements from device
-  //  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  // Requires DMP to be ready.
   if (!dmpReady) return;
+  // Grab the current DMP packet
   if (!mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) return;
+  // Tick the BLE subsystem, processing any device connections/disconnections that need to happen.
+//  bleTick();
+
+  // Extract the pitch angle from the DMP packet
   Quaternion q;
   VectorFloat gravity;
   float ypr[3];
   mpu.dmpGetQuaternion(&q, fifoBuffer);
   mpu.dmpGetGravity(&gravity, &q);
   mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-//  Serial.print("ypr\t");
-//  Serial.print(ypr[0] * 180 / M_PI);
-//  Serial.print("\t");
-//  Serial.print(ypr[1] * 180 / M_PI);
-//  Serial.print("\t");
-//  Serial.print(ypr[2] * 180 / M_PI);
   
-  // Turn motors proportional to x accel
+  // Feed pitch angle to the PID controller
   float requested = controller.calcPid(ypr[1], (millis() - lastMillis) / 1000.0f);
   delay(2);
-  int amt = requested;// constrain(requested, -255, 255);
-//  float n = analogRead(PIN_TRIMPOT);
-//  float d = analogRead(PIN_DTRIM);
-//
-//  controller.p = mapf(n, 0.0, 4095.0, 0, 3000);
-//  controller.d = mapf(d, 0.0, 4095.0, 0, 2000);
-  
-//  Serial.printf("n: %f, d: %f\n", n, d);
-
-//  Serial.print("Err: "); Serial.print(requested); Serial.print(", p: "); Serial.print(controller.p); Serial.print(", d: "); Serial.println(controller.d);
   Serial.printf("E: %f\n", requested);
-  
-  left.write(amt);
-  right.write(amt);
-  bleTick();
+
+  // Forward PID controller's request to the motors.
+  left.write(requested);
+  right.write(requested);
 }
